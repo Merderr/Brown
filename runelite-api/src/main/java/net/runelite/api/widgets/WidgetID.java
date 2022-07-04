13eaeb9ff1f8fd87843b67ad049012f8f0046fb5
Hamza <hamzasahmed11@gmail.com>
2022-07-04 17:40:16 -0400

    specbarupdated

--- runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
+++ runelite-api/src/main/java/net/runelite/api/widgets/WidgetID.java
@@ -596,6 +596,7 @@ public final class WidgetID
 		static final int SPELL_ICON = 28;
 		static final int SPELL_TEXT = 29;
 		static final int AUTO_RETALIATE = 30;
+		static final int SPECIAL_ATTACK_BAR = 34;
 	}
 
 	static class VolcanicMine
